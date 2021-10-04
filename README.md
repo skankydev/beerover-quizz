292ce0578122ee8577efc01a5ad73da67024ffb4
simon schenck <simon.schenck@yakabiz.com>
2021-10-04 20:11:21 +0200

    des test

--- README.md
+++ README.md
@@ -3,10 +3,16 @@ _(l application pour aller avec le buzzer)_
 
 [![Generic badge](https://img.shields.io/badge/BeerOver-Quizz-green.svg)](https://shields.io/)
 
-l application pour qui va avec le buzzer.
+l application pour qui va avec le buzzer. api web bleutooth que sur chrome.
 
 
 ## Pour commencer
 
-de la patience
+coming soon
+
+## Truc utile
+
+
+chrome://flags/#enable-web-bluetooth-new-permissions-backend
+
 DevTools shortcut "Ctrl+Shift+I" 
--- src/js/quizz.js
+++ src/js/quizz.js
@@ -6,3 +6,38 @@ window.Vue = Vue;
 Vue.component('beerover-app', BeerOverApp);
 
 console.log('Coucou');
+var deviceName = 'Palm'
+
+function isWebBluetoothEnabled() {
+	if (!navigator.bluetooth) {
+		console.log('Web Bluetooth API is not available in this browser!')
+		return false
+	}
+
+	return true
+}
+
+function getDeviceInfo() {
+	let options = {
+		acceptAllDevices: true // Option to accept all devices
+		/*"filters": [
+			{ "name": deviceName }
+		]*/
+	}
+
+	console.log('Requesting Bluetooth Device...')
+	navigator.bluetooth.requestDevice(options).then(device => {
+		console.log('> Name: ' + device.name)
+	}).catch(error => {
+		console.log('Argh! ' + error)
+	})
+}
+
+document.querySelector('form').addEventListener('submit', function(event) {
+	event.stopPropagation()
+	event.preventDefault()
+
+	getDeviceInfo()
+	/*if (isWebBluetoothEnabled()) {
+	}*/
+})
