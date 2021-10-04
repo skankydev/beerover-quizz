de5434fb4401a6f2d6259f2758280705491067ce
simon schenck <simon.schenck@yakabiz.com>
2021-10-04 22:53:49 +0200

    re test

--- src/js/quizz.js
+++ src/js/quizz.js
@@ -6,38 +6,104 @@ window.Vue = Vue;
 Vue.component('beerover-app', BeerOverApp);
 
 console.log('Coucou');
-var deviceName = 'Palm'
+
+var deviceName = 'beerover'
+var bleService = 'environmental_sensing'
+var bleCharacteristic = 'uv_index'
+var bluetoothDeviceDetected
+var gattCharacteristic
+
+document.querySelector('#read').addEventListener('click', function() {
+	if (isWebBluetoothEnabled()) { read() }
+})
+
+document.querySelector('#start').addEventListener('click', function(event) {
+	if (isWebBluetoothEnabled()) { start() }
+})
+
+document.querySelector('#stop').addEventListener('click', function(event) {
+	if (isWebBluetoothEnabled()) { stop() }
+})
 
 function isWebBluetoothEnabled() {
 	if (!navigator.bluetooth) {
 		console.log('Web Bluetooth API is not available in this browser!')
 		return false
 	}
-
 	return true
 }
 
 function getDeviceInfo() {
 	let options = {
-		acceptAllDevices: true // Option to accept all devices
-		/*"filters": [
+		optionalServices: [bleService],
+		filters: [
 			{ "name": deviceName }
-		]*/
+		]
 	}
 
-	console.log('Requesting Bluetooth Device...')
-	navigator.bluetooth.requestDevice(options).then(device => {
-		console.log('> Name: ' + device.name)
+	console.log('Requesting any Bluetooth Device...')
+	return navigator.bluetooth.requestDevice(options).then(device => {
+		bluetoothDeviceDetected = device
 	}).catch(error => {
 		console.log('Argh! ' + error)
 	})
 }
 
-document.querySelector('form').addEventListener('submit', function(event) {
-	event.stopPropagation()
-	event.preventDefault()
+function read() {
+	return (bluetoothDeviceDetected ? Promise.resolve() : getDeviceInfo())
+	.then(connectGATT)
+	.then(_ => {
+		console.log('Reading UV Index...')
+		return gattCharacteristic.readValue()
+	}).catch(error => {
+		console.log('Waiting to start reading: ' + error)
+	})
+}
 
-	getDeviceInfo()
-	/*if (isWebBluetoothEnabled()) {
-	}*/
-})
+function connectGATT() {
+	if (bluetoothDeviceDetected.gatt.connected && gattCharacteristic) {
+		return Promise.resolve()
+	}
+
+	return bluetoothDeviceDetected.gatt.connect().then(server => {
+		console.log('Getting GATT Service...')
+		return server.getPrimaryService(bleService)
+	}).then(service => {
+		console.log('Getting GATT Characteristic...')
+		return service.getCharacteristic(bleCharacteristic)
+	}).then(characteristic => {
+		gattCharacteristic = characteristic
+		gattCharacteristic.addEventListener('characteristicvaluechanged',
+		handleChangedValue)
+		document.querySelector('#start').disabled = false
+		document.querySelector('#stop').disabled = true
+	})
+}
+
+function handleChangedValue(event) {
+	let value = event.target.value.getUint8(0)
+	var now = new Date()
+	console.log('> ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds() + ' UV Index is ' + value)
+}
+
+function start() {
+	gattCharacteristic.startNotifications()
+	.then(_ => {
+		console.log('Start reading...')
+		document.querySelector('#start').disabled = true
+		document.querySelector('#stop').disabled = false
+	}).catch(error => {
+		console.log('[ERROR] Start: ' + error)
+	})
+}
+
+function stop() {
+	gattCharacteristic.stopNotifications()
+	.then(_ => {
+		console.log('Stop reading...')
+		document.querySelector('#start').disabled = false
+		document.querySelector('#stop').disabled = true
+	}).catch(error => {
+		console.log('[ERROR] Stop: ' + error)
+	})
+}
